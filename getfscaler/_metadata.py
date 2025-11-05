312cc6e3f1fa5894a001a6587b094ecb66c98322
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-11-06 00:44:50 +0100

    bump minor version

--- getfscaler/_metadata.py
+++ getfscaler/_metadata.py
@@ -1,6 +1,6 @@
 """Tool to help find the kernel used for upscaling non-source resolution native images using Vapoursynth."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __author__ = "LightArrowsEXE <LightArrowsReboot@gmail.com>"
 __maintainer__ = __author__
